132169e265cd53651f0e52ccb03b8e35dd8769bf
kdygit <2353723277@qq.com>
2023-03-18 17:55:06 +0800

    second commit git

--- src/main/java/com/kdy/git/GitDemo.java
+++ src/main/java/com/kdy/git/GitDemo.java
@@ -3,5 +3,6 @@ package com.kdy.git;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
